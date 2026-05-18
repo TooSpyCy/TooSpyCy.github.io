a111e1e725d791af591a71199fee01d90e6d2c84
EtriZe <valentin.barrerepro@gmail.com>
2026-05-19 00:38:48 +0200

    Config supabase

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -4,8 +4,8 @@
 // Settings > API dans le dashboard Supabase
 // ============================================================
 
-const SUPABASE_URL      = 'https://TON-PROJET.supabase.co';
-const SUPABASE_ANON_KEY = 'TA_CLE_ANON_PUBLIQUE';
+const SUPABASE_URL      = 'https://trayertrmajocnfrengj.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_KLjT25yOxTILq4YKeiJUtw_0JteKl2m';
 
 const { createClient } = supabase;
 const supabaseClient   = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
